79bf7ebf10ab87cb99a6d56f8baa69af8dc4ee5c
nathan <nguyendinhnhat206@gmail.com>
2025-12-12 13:22:43 -0500

    -- Added favicon

--- Models/ViewModels/PagingInfo.cs
+++ Models/ViewModels/PagingInfo.cs
@@ -5,6 +5,11 @@ public class PagingInfo
     public int TotalItems { get; set; }
     public int ItemsPerPage { get; set; }
     public int CurrentPage { get; set; }
-    public int TotalPages =>
-        (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
+    
+
+    public int StartPage { get; private set; }
+    public int EndPage { get; private set; }
+    public bool HasPreviousPage => CurrentPage > 1;
+    public bool HasNextPage => CurrentPage < TotalPages;
+    public int TotalPages => (int)Math.Ceiling((decimal)TotalItems / ItemsPerPage);
 }
